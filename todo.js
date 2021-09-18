68c9caca26b0a38a94a038865da36cb76c2a1ed5
joshuaalexanderio <joshuaalexandertb@gmail.com>
2021-09-18 08:40:22 -0500

    Todo! banner now prompts for input on click

--- todo.js
+++ todo.js
@@ -4,32 +4,34 @@ function displayList() {
 	alert("-" + list.join("\n-"));
 }
 
-while (input.toLowerCase() !== "quit") {
-	input = prompt("What would you like to do?");
-	if (input.toLowerCase() === "add") {
-		input = prompt("Enter item to be added: \n");
-		list.push(input);
-		alert("Updated List:\n-" + list.join("\n-"));
-	} else if (input.toLowerCase() === "list") {
-		displayList();
-	} else if (input.toLowerCase() === "remove") {
-		let item = prompt("-" + list.join("\n-") + "\nEnter item to be deleted: \n");
-		list.splice(list.indexOf(item), 1);
-	} else if (input.toLowerCase() === "sort") {
-		// sort list using bubble sort algorithm
-		var swapped = true;
-		while (swapped === true) {
-			swapped = false;
-			for (let i = 0; i < list.length; i++) {
-				if (list[i] > list[i + 1]) {
-					swapped = true; // item was swapped this pass
-					let temp = list[i];
-					list[i] = list[i + 1];
-					list[i + 1] = temp;
+function getInput() {
+	while (input.toLowerCase() !== "quit") {
+		input = prompt("What would you like to do?");
+		if (input.toLowerCase() === "add") {
+			input = prompt("Enter item to be added: \n");
+			list.push(input);
+			alert("Updated List:\n-" + list.join("\n-"));
+		} else if (input.toLowerCase() === "list") {
+			displayList();
+		} else if (input.toLowerCase() === "remove") {
+			let item = prompt("-" + list.join("\n-") + "\nEnter item to be deleted: \n");
+			list.splice(list.indexOf(item), 1);
+		} else if (input.toLowerCase() === "sort") {
+			// sort list using bubble sort algorithm
+			var swapped = true;
+			while (swapped === true) {
+				swapped = false;
+				for (let i = 0; i < list.length; i++) {
+					if (list[i] > list[i + 1]) {
+						swapped = true; // item was swapped this pass
+						let temp = list[i];
+						list[i] = list[i + 1];
+						list[i + 1] = temp;
+					}
 				}
 			}
+		} else {
+			alert("Invalid Usage");
 		}
-	} else {
-		alert("Invalid Usage");
 	}
 }
