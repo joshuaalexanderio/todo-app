4021c5ddca17c929bbb77c1c79b4e0682af1de02
joshuaalexanderio <joshuaalexandertb@gmail.com>
2021-09-18 06:48:00 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Todo App Project :)
+
+## This is a very simple todo app using browser alerts as an iterface. I intend to update this with a graphical interface once I learn the DOM.
---
+++ todo.js
@@ -0,0 +1,31 @@
+var input = "";
+var list = [];
+while (input.toLowerCase() !== "quit") {
+	input = prompt("What would you like to do?");
+	if (input.toLowerCase() === "add") {
+		input = prompt("Enter item to be added: \n");
+		list.push(input);
+		alert("Updated List:\n-" + list.join("\n-"));
+	} else if (input.toLowerCase() === "list") {
+		alert("-" + list.join("\n-"));
+	} else if (input.toLowerCase() === "remove") {
+		let item = prompt("-" + list.join("\n-") + "\nEnter item to be deleted: \n");
+		list.splice(list.indexOf(item), 1);
+	} else if (input.toLowerCase() === "sort") {
+		// sort list using bubble sort algorithm
+		var swapped = true;
+		while (swapped === true) {
+			swapped = false;
+			for (let i = 0; i < list.length; i++) {
+				if (list[i] > list[i + 1]) {
+					swapped = true; // item was swapped this pass
+					let temp = list[i];
+					list[i] = list[i + 1];
+					list[i + 1] = temp;
+				}
+			}
+		}
+	} else {
+		alert("Invalid Usage");
+	}
+}
