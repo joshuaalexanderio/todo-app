ea7059abc579071ef00b5b8b5080f97f2e64654c
joshuaalexanderio <joshuaalexandertb@gmail.com>
2021-09-20 17:23:13 -0500

    Added comments

--- todo.js
+++ todo.js
@@ -1,48 +1,10 @@
-function displayList(list) {
-  if (list.length === 0) {
-    alert("Your list is empty :)");
-    getInput();
-  }
-  alert("-" + list.join("\n-"));
-}
-// Add list item
-function addItem(list, listItem) {
-  // If no input, re-prompt
-  if (listItem === "") {
-    getInput();
-  }
-  // Add input to end of list array
-  list.push(listItem);
-  alert("Updated List:\n-" + list.join("\n-"));
-}
-// Remove list item
-function removeItem(list, listItem) {
-  list.splice(list.indexOf(listItem), 1);
-}
-
-// sort list using bubble sort
-function sort(list, listItem) {
-  var swapped = true;
-  while (swapped === true) {
-    swapped = false;
-    for (let i = 0; i < list.length; i++) {
-      if (list[i] > list[i + 1]) {
-        swapped = true; // item was swapped this pass
-        let temp = list[i];
-        list[i] = list[i + 1];
-        list[i + 1] = temp;
-      }
-    }
-  }
-}
-
 function getInput() {
   let list = [];
   let input = "";
   do {
     let input = prompt("What would you like to do?");
-    let command = input.split(" ")[0];
-    let listItem = input.split(" ").splice(1).join(" ");
+    let command = input.split(" ")[0]; // Isolate to-do item from input
+    let listItem = input.split(" ").splice(1).join(" "); // Isolate command from input
     switch (command) {
       case "add":
         addItem(list, listItem);
@@ -67,4 +29,41 @@ function getInput() {
         alert("Invalid Usage: Please use valid single command arugment.");
     }
   } while (input !== "quit");
+}
+// Display list if not empty
+function displayList(list) {
+  if (list.length === 0) {
+    alert("Your list is empty :)");
+    getInput();
+  }
+  alert("-" + list.join("\n-")); // Display list
+}
+// Add list item
+function addItem(list, listItem) {
+  // If no input, re-prompt
+  if (listItem === "") {
+    getInput();
+  }
+  list.push(listItem); // Add input to end of list array
+  alert("Updated List:\n-" + list.join("\n-"));
+}
+// Remove list item
+function removeItem(list, listItem) {
+  list.splice(list.indexOf(listItem), 1);
+}
+
+// sort list using bubble sort
+function sort(list, listItem) {
+  var swapped = true;
+  while (swapped === true) {
+    swapped = false;
+    for (let i = 0; i < list.length; i++) {
+      if (list[i] > list[i + 1]) {
+        swapped = true; // item was swapped this pass
+        let temp = list[i];
+        list[i] = list[i + 1];
+        list[i + 1] = temp;
+      }
+    }
+  }
 }
