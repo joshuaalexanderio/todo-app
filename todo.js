381ed04fdfad0785154b3cfb02f2cf9b45d42859
joshuaalexanderio <joshuaalexandertb@gmail.com>
2021-09-20 17:18:08 -0500

    *Created external functions for all commands

--- todo.js
+++ todo.js
@@ -1,15 +1,27 @@
-var input = "";
-var list = [];
-function displayList() {
+function displayList(list) {
   if (list.length === 0) {
     alert("Your list is empty :)");
     getInput();
   }
   alert("-" + list.join("\n-"));
 }
+// Add list item
+function addItem(list, listItem) {
+  // If no input, re-prompt
+  if (listItem === "") {
+    getInput();
+  }
+  // Add input to end of list array
+  list.push(listItem);
+  alert("Updated List:\n-" + list.join("\n-"));
+}
+// Remove list item
+function removeItem(list, listItem) {
+  list.splice(list.indexOf(listItem), 1);
+}
 
 // sort list using bubble sort
-function sort(list) {
+function sort(list, listItem) {
   var swapped = true;
   while (swapped === true) {
     swapped = false;
@@ -23,20 +35,17 @@ function sort(list) {
     }
   }
 }
+
 function getInput() {
+  let list = [];
+  let input = "";
   do {
-    input = prompt("What would you like to do?");
+    let input = prompt("What would you like to do?");
     let command = input.split(" ")[0];
     let listItem = input.split(" ").splice(1).join(" ");
     switch (command) {
       case "add":
-        // If no input, re-prompt
-        if (listItem === "") {
-          getInput();
-        }
-        // Add input to end of list array
-        list.push(listItem);
-        alert("Updated List:\n-" + list.join("\n-"));
+        addItem(list, listItem);
         break;
       case "remove":
         // If item does not exist, break
@@ -44,10 +53,10 @@ function getInput() {
           alert("List item does not exist.");
           break;
         }
-        list.splice(list.indexOf(listItem), 1);
+        removeItem(list, listItem);
         break;
       case "list":
-        displayList();
+        displayList(list);
         break;
       case "sort":
         sort(list);
