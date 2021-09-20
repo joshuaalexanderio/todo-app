5aff019a97f4ee0d6426c7e576bbd2b19e87afe6
joshuaalexanderio <joshuaalexandertb@gmail.com>
2021-09-20 01:54:11 -0500

    *Refactor logic, create sort function, misc bugfix
    -Do...while statement changed to switch statement for performance and readbility.
    -Created external function for sort functionality.
    -Added code for when item to be removed does not exist.

--- todo.js
+++ todo.js
@@ -1,40 +1,61 @@
 var input = "";
 var list = [];
 function displayList() {
+	if (list.length === 0) {
+		alert("Your list is empty :)");
+		getInput();
+	}
 	alert("-" + list.join("\n-"));
 }
 
+// sort list using bubble sort
+function sort(list) {
+	var swapped = true;
+	while (swapped === true) {
+		swapped = false;
+		for (let i = 0; i < list.length; i++) {
+			if (list[i] > list[i + 1]) {
+				swapped = true; // item was swapped this pass
+				let temp = list[i];
+				list[i] = list[i + 1];
+				list[i + 1] = temp;
+			}
+		}
+	}
+}
 function getInput() {
 	do {
-		input = prompt("What would you like to do?");
-		if (input.toLowerCase() === "add") {
-			input = prompt("Enter item to be added: \n");
-			list.push(input);
-			alert("Updated List:\n-" + list.join("\n-"));
-		} else if (input.toLowerCase() === "list") {
-			//
-			displayList();
-		} else if (input.toLowerCase() === "remove") {
-			let item = prompt("-" + list.join("\n-") + "\nEnter item to be deleted: \n");
-			list.splice(list.indexOf(item), 1);
-		} else if (input.toLowerCase() === "sort") {
-			// sort list using bubble sort algorithm
-			var swapped = true;
-			while (swapped === true) {
-				swapped = false;
-				for (let i = 0; i < list.length; i++) {
-					if (list[i] > list[i + 1]) {
-						swapped = true; // item was swapped this pass
-						let temp = list[i];
-						list[i] = list[i + 1];
-						list[i + 1] = temp;
-					}
+		switch (prompt("What would you like to do?")) {
+			case "add":
+				input = prompt("Enter item to be added: \n");
+				//If no input, re-prompt
+				if (input.split(" ").join("") === "") {
+					getInput();
 				}
-			}
-		} else if (input.toLowerCase() === "quit") {
-			break;
-		} else {
-			alert("Invalid Usage. Please use single valid command argument.");
+				list.push(input); // Add input to end of list array
+				alert("Updated List:\n-" + list.join("\n-"));
+				break;
+			case "list":
+				displayList();
+				break;
+			case "remove":
+				let item = prompt(
+					"-" + list.join("\n-") + "\nEnter item to be deleted: \n"
+				);
+				if (list.indexOf(item) === -1) {
+					console.log("It worked");
+					alert("List item does not exist.");
+					break;
+				}
+				list.splice(list.indexOf(item), 1);
+				break;
+			case "sort":
+				sort(list);
+				break;
+			case "quit":
+				return;
+			default:
+				alert("Invalid Usage: Please use valid single command arugment.");
 		}
-	} while (input.toLowerCase() !== "quit");
+	} while (input !== "quit");
 }
